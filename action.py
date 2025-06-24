aacb024bae3b0a28a2efa9bf44a9c8d5a263c23d
til <m15071452418@163.com>
2025-06-24 21:10:20 +0800

    add

--- action.py
+++ action.py
@@ -20,7 +20,7 @@ class ActionComponent(Component):
         self.actionClient = actionlib.SimpleActionClient("move_base", MoveBaseAction)
         self.actionClient.wait_for_server()
 
-        await asyncio.create_task(self.instructionLoop())
+        asyncio.create_task(self.instructionLoop())
 
     async def actionNav(self, x_axle=1, y_axle=0, x=0, y=0, z=0, w=1.0):
         goal = MoveBaseGoal()
