ff3da9709f3796b3c6865402585f8b9dce22fbbd
til <m15071452418@163.com>
2025-06-24 21:01:13 +0800

    add

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ import os
 import datetime
 
 print("=" * 40, "Environment Information", "=" * 40)
-print(f"Python Version: {sys.version}")
-print(f"Python Executable: {sys.executable}")
-print(f"OS Platform: {platform.platform()}")
-print(f"System: {platform.system()} {platform.release()}")
-print(f"Processor: {platform.processor()}")
-print(f"Current Time: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-print(f"Working Directory: {os.getcwd()}")
-print(f"Username: {os.getlogin()}")
-print(f"Machine Name: {platform.node()}")
+
+try:
+    print(f"Python Version: {sys.version}")
+    print(f"Python Executable: {sys.executable}")
+    print(f"OS Platform: {platform.platform()}")
+    print(f"System: {platform.system()} {platform.release()}")
+    print(f"Processor: {platform.processor()}")
+    print(f"Current Time: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    print(f"Working Directory: {os.getcwd()}")
+    print(f"Username: {os.getlogin()}")
+    print(f"Machine Name: {platform.node()}")
+except Exception as e:
+    print(e)
+
 print("=" * 100)
 
 
